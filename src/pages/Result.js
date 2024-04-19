060b87257ef0735563084b11ef446054d54e15f9
서원빈 <1bins@kakao.com>
2024-04-19 11:56:26 +0900

    :sparkles: 결과 페이지 점수 별 결과지 문구 추가

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,6 +1,19 @@
+import { productList } from "../productList";
+
 const Result = ({ totalScore }) => {
+    const getMilkis = (score) => {
+        if(score <= 22) return productList[0]
+        else if(score >= 23 && score <= 37) return productList[1]
+        else if(score >= 38) return productList[2]
+    }
+
     return(
-    <div>결과 페이지~ <br/>총 점수는 <b>{totalScore}</b></div>
+        <div>
+            <p>총 점수는 <b>{totalScore}</b></p>
+            <p>{getMilkis(totalScore).name}</p>
+            <p>{getMilkis(totalScore).title}</p>
+            <p>{getMilkis(totalScore).content}</p>
+        </div>
     )
 }
 
